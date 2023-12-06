3754fa60f60786c23e084752580a9be0b19b1dfa
서기표 <gpyo0111@naver.com>
2023-12-06 12:43:03 +0900

    서기표

--- app/src/main/java/com/example/ootdfinalapplication/sourchFragment.kt
+++ app/src/main/java/com/example/ootdfinalapplication/sourchFragment.kt
@@ -9,30 +9,8 @@ import androidx.navigation.fragment.findNavController
 import com.example.ootdfinalapplication.databinding.FragmentAdd2Binding
 import com.example.ootdfinalapplication.databinding.FragmentSourchBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-/*
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
- */
-
 class sourchFragment : Fragment() {
-    /*
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-     */
     var binding : FragmentSourchBinding? = null
 
     override fun onCreateView(
@@ -43,28 +21,7 @@ class sourchFragment : Fragment() {
         // Inflate the layout for this fragment
         return binding?.root
     }
-    /*
-        companion object {
-            /**
-             * Use this factory method to create a new instance of
-             * this fragment using the provided parameters.
-             *
-             * @param param1 Parameter 1.
-             * @param param2 Parameter 2.
-             * @return A new instance of fragment add2Fragment.
-             */
-            // TODO: Rename and change types and number of parameters
-            @JvmStatic
-            fun newInstance(param1: String, param2: String) =
-                add2Fragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-        }
 
-     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
